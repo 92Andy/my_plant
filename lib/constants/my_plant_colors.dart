b1ed758844c32cd9442fec6ec82b0949f86ba975
Helena Eckhardt <helenakeller92@gmail.com>
2023-01-02 21:59:20 +0100

    Added app colors

---
+++ lib/constants/my_plant_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MyPlantColors {
+  static Color berylGreen = const Color(0x00dbe5be);
+  static Color deco = const Color(0x00c0d58b);
+  static Color greenSmoke = const Color(0x009daf6c);
+  static Color costaDelSol = const Color(0x00637337);
+}
