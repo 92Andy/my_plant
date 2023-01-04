09462bac72aaa1c4c9a65a78006ec3300643045e
Andy Eckhardt <eckhardtandy@gmail.com>
2023-01-04 21:30:08 +0100

    #2: custom colors fix

--- lib/constants/my_plant_colors.dart
+++ lib/constants/my_plant_colors.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MyPlantColors {
-  static Color berylGreen = const Color(0x00dbe5be);
-  static Color deco = const Color(0x00c0d58b);
-  static Color greenSmoke = const Color(0x009daf6c);
-  static Color costaDelSol = const Color(0x00637337);
+  static Color berylGreen = const Color(0xffdbe5be);
+  static Color deco = const Color(0xffc0d58b);
+  static Color greenSmoke = const Color(0xff9daf6c);
+  static Color costaDelSol = const Color(0xff637337);
   static Color surfaceWhite = const Color(0xfff8f8f8);
-  static Color textBlack = const Color(0x00323232);
+  static Color textBlack = const Color(0xff323232);
 }
