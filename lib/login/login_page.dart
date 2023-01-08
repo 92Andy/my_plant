dcd7abe1aad9ebc94d6da900d110080a028bdb45
Andy Eckhardt <eckhardtandy@gmail.com>
2023-01-08 19:36:10 +0100

    #3: top left foreground shape added

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_plant/login/widgets/login_button.dart';
+import 'package:my_plant/constants/my_plant_colors.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -7,11 +7,65 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: LoginButton(
-          onLoginClicked: () {},
+        body: Column(
+      children: const [
+        SizedBox(
+          height: 100,
         ),
+        TopLeftLoginForegroundShape(),
+      ],
+    ));
+  }
+}
+
+class TopLeftLoginForegroundShape extends StatelessWidget {
+  const TopLeftLoginForegroundShape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: _TopLeftLoginForegroundShapeClipper(),
+      child: Container(
+        height: 200,
+        width: MediaQuery.of(context).size.width,
+        color: MyPlantColors.deco,
       ),
     );
   }
+}
+
+class _TopLeftLoginForegroundShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final shapeHeightMaxY = size.height - 100;
+    final shapeStartPointX = size.width * .5;
+    Path path = Path()
+      ..moveTo(shapeStartPointX, 0)
+      ..quadraticBezierTo(
+        shapeStartPointX + 95,
+        size.height * .2,
+        shapeStartPointX + 70,
+        shapeHeightMaxY,
+      )
+      ..quadraticBezierTo(
+        shapeStartPointX + 60,
+        size.height - 55,
+        shapeStartPointX + 140,
+        size.height - 90,
+      )
+      ..quadraticBezierTo(
+        size.width - 30,
+        size.height - 100,
+        size.width,
+        size.height - 95,
+      )
+      ..lineTo(size.width, 0)
+      ..close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) =>
+      true; //For debugging
 }
