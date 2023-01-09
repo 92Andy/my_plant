2c463de6055905995e2a5c514248a395ed499bf3
Andy Eckhardt <eckhardtandy@gmail.com>
2023-01-09 20:59:04 +0100

    #3: Added top background shape

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -7,15 +7,60 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: const [
-        SizedBox(
-          height: 100,
-        ),
-        TopLeftLoginForegroundShape(),
-      ],
-    ));
+      body: Stack(
+        children: const [
+          TopLeftLoginBackgroundShape(),
+          TopLeftLoginForegroundShape(),
+        ],
+      ),
+    );
   }
+}
+
+class TopLeftLoginBackgroundShape extends StatelessWidget {
+  const TopLeftLoginBackgroundShape({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: _TopLeftLoginBackgroundShapeClipper(),
+      clipBehavior: Clip.antiAlias,
+      child: Container(
+        height: 200,
+        width: MediaQuery.of(context).size.width,
+        color: MyPlantColors.greenSmoke,
+      ),
+    );
+  }
+}
+
+class _TopLeftLoginBackgroundShapeClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    final shapeHeightMaxY = size.height * .7;
+    final shapeStartPointX = size.width * .3;
+    Path path = Path()
+      ..moveTo(shapeStartPointX, 0)
+      ..quadraticBezierTo(
+        shapeStartPointX * 1,
+        size.height * .6,
+        shapeStartPointX * 2,
+        shapeHeightMaxY * .6,
+      )
+      ..quadraticBezierTo(
+        size.width * .9,
+        size.height * .3,
+        size.width,
+        shapeHeightMaxY,
+      )
+      ..lineTo(size.width, 0)
+      ..close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) => true;
 }
 
 class TopLeftLoginForegroundShape extends StatelessWidget {
@@ -37,27 +82,27 @@ class TopLeftLoginForegroundShape extends StatelessWidget {
 class _TopLeftLoginForegroundShapeClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
-    final shapeHeightMaxY = size.height - 100;
+    final shapeHeightMaxY = size.height * .5;
     final shapeStartPointX = size.width * .5;
     Path path = Path()
       ..moveTo(shapeStartPointX, 0)
       ..quadraticBezierTo(
-        shapeStartPointX + 95,
+        shapeStartPointX * 1.45,
         size.height * .2,
-        shapeStartPointX + 70,
+        shapeStartPointX * 1.35,
         shapeHeightMaxY,
       )
       ..quadraticBezierTo(
-        shapeStartPointX + 60,
-        size.height - 55,
-        shapeStartPointX + 140,
-        size.height - 90,
+        shapeStartPointX * 1.25,
+        size.height * .7,
+        shapeStartPointX * 1.7,
+        size.height * .5,
       )
       ..quadraticBezierTo(
-        size.width - 30,
-        size.height - 100,
+        size.width * .9,
+        size.height * .45,
         size.width,
-        size.height - 95,
+        size.height * .5,
       )
       ..lineTo(size.width, 0)
       ..close();
