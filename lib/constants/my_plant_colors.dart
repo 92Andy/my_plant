7bb5e2415f16f0faefa4221b13bafb783991bd6f
Andy Eckhardt <eckhardtandy@gmail.com>
2023-01-16 22:01:22 +0100

    #12: color improved

--- lib/constants/my_plant_colors.dart
+++ lib/constants/my_plant_colors.dart
@@ -4,7 +4,7 @@ class MyPlantColors {
   static Color berylGreen = const Color(0xffdbe5be);
   static Color deco = const Color(0xffc0d58b);
   static Color greenSmoke = const Color(0xff9daf6c);
-  static Color costaDelSol = const Color(0xff637337);
+  static Color costaDelSol = const Color(0xff88A64C);
   static Color surfaceWhite = const Color(0xfff8f8f8);
   static Color textBlack = const Color(0xff323232);
   static Color lightGrey = const Color(0xffc6c6c6);
