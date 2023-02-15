ece5b0f5dc514990cef037896eb1c162cb9115b7
Andy Eckhardt <eckhardtandy@gmail.com>
2023-02-15 21:03:16 +0100

    #12: Added dummy list items and background tap container

--- lib/my_plants/my_plants_page.dart
+++ lib/my_plants/my_plants_page.dart
@@ -1,6 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:my_plant/constants/my_plant_colors.dart';
 
+final _plants = <String>[
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+];
+
 class MyPlantsPage extends StatefulWidget {
   const MyPlantsPage({super.key});
 
@@ -36,12 +49,14 @@ class _MyPlantsPageState extends State<MyPlantsPage>
       builder: (context, child) {
         final rightTranslation =
             rightTransformationPosX * _animationController.value;
-        final scaling = 1 - (_animationController.value * 0.3);
+        final scaling = 1 - (_animationController.value * 0.15);
 
         return Stack(
           children: [
-            const Scaffold(
-              body: _CustomDrawer(),
+            Scaffold(
+              body: _CustomDrawer(
+                plants: _plants,
+              ),
             ),
             Transform(
               transform: Matrix4.identity()
@@ -107,6 +122,14 @@ class _MyPlantDetailPage extends StatelessWidget {
           topLeft: Radius.circular(
               _borderCircularRadius * animationController.value),
         ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 1,
+            blurRadius: 2,
+            offset: const Offset(0, 0), // changes position of shadow
+          ),
+        ],
       ),
       child: Center(
         child: OutlinedButton(
@@ -119,7 +142,11 @@ class _MyPlantDetailPage extends StatelessWidget {
 }
 
 class _CustomDrawer extends StatelessWidget {
-  const _CustomDrawer();
+  const _CustomDrawer({
+    required this.plants,
+  });
+
+  final List<String> plants;
 
   @override
   Widget build(BuildContext context) {
@@ -132,7 +159,7 @@ class _CustomDrawer extends StatelessWidget {
             height: MediaQuery.of(context).size.height,
             decoration: BoxDecoration(
               color: MyPlantColors.greenSmoke,
-              borderRadius: BorderRadius.circular(20),
+              borderRadius: BorderRadius.circular(30),
             ),
             child: Column(
               children: [
@@ -150,6 +177,16 @@ class _CustomDrawer extends StatelessWidget {
                   color: Colors.white,
                   thickness: 3,
                 ),
+                Expanded(
+                  child: ListView(
+                    children: List.generate(
+                      plants.length,
+                      (index) {
+                        return _PlantCard(title: plants[index]);
+                      },
+                    ),
+                  ),
+                )
               ],
             ),
           ),
@@ -173,6 +210,73 @@ class _CustomDrawer extends StatelessWidget {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class _PlantCard extends StatefulWidget {
+  const _PlantCard({
+    required this.title,
+  });
+
+  final String title;
+
+  @override
+  State<_PlantCard> createState() => _PlantCardState();
+}
+
+class _PlantCardState extends State<_PlantCard> {
+  final _diameter = 40.0;
+  final _tappedBorderRadius = 20.0;
+  bool _tapped = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => setState(() {
+        _tapped = !_tapped;
+      }),
+      child: SizedBox(
+        height: 100,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            if (_tapped)
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(_tappedBorderRadius),
+                    bottomLeft: Radius.circular(_tappedBorderRadius),
+                  ),
+                  color: MyPlantColors.myPlantWhite,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 1,
+                      blurRadius: 2,
+                      offset: const Offset(0, 0), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+            Container(
+              height: _diameter,
+              width: _diameter,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(_diameter),
+                color: Colors.white,
+              ),
+            ),
+            Text(
+              widget.title,
+              style: const TextStyle(
+                fontSize: 15,
+                fontWeight: FontWeight.bold,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
